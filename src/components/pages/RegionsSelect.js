ab5a54457b78751cdd0c7947245f450f59c47820
Cosmin Oance <cosmin.oance@roxorgaming.com>
2021-02-26 15:54:48 +0000

    fixing state

--- src/components/pages/RegionsSelect.js
+++ src/components/pages/RegionsSelect.js
@@ -9,6 +9,7 @@ class RegionsSelect extends Component {
         this.baselineDetails = props.baseline;
         this.regionRenderer = this.regionRenderer.bind(this);
         this.onChange = this.onChange.bind(this);
+        //get existing ignore blocks
         this.existingIgnoreBlokcs = [];
         if (this.baselineDetails
             && this.baselineDetails.ignoreBoxes
@@ -20,11 +21,12 @@ class RegionsSelect extends Component {
                         'width':(100-block.left)-(100-block.right),
                         'height':(100-block.bottom)-(100-block.top)
                     }
-                )
+                );
             });
         }
-        this.state={};
-        this.state.regions = this.existingIgnoreBlokcs;
+        this.state = {
+            regions:[]  //< putting this.existingIgnoreBlocks here breaks it
+        };
         this.imgSrc = this.props.image;
         this.nrOfRegions = parseInt(this.props.nrOfRegions);
         this.ignoreBlocks = null;
@@ -86,14 +88,16 @@ class RegionsSelect extends Component {
             background: 'rgba(0, 255, 80, 0.3)'
         };
         this.ignoreBlocks = [];
-        this.state.regions.forEach(crtRegion=> {
-            this.ignoreBlocks.push({
-                'left':crtRegion.x,
-                'top':crtRegion.y,
-                'right':100-(crtRegion.x+crtRegion.width),
-                'bottom':100-(crtRegion.y+crtRegion.height)
-            })});
-
+        if(this.state != null) {
+            this.state.regions.forEach(crtRegion => {
+                this.ignoreBlocks.push({
+                    'left': crtRegion.x,
+                    'top': crtRegion.y,
+                    'right': 100 - (crtRegion.x + crtRegion.width),
+                    'bottom': 100 - (crtRegion.y + crtRegion.height)
+                })
+            });
+        }
         return (
             <div style={{ display: 'block' }}>
                 <div style={{ flexGrow: 1, flexShrink: 1, width: '100%' }}>
@@ -109,7 +113,7 @@ class RegionsSelect extends Component {
                         <img src={this.imgSrc} alt = 'your baseline' id = 'baseline' width='100%'/>
                     </RegionSelect>
                 </div>
-                <p>Ignored Regions <button onMouseUp={(event)=>{this.onSaveToBaseline(this.baselineDetails,event)}}>Save to Baseline</button>
+                <p>Ignored Regions <button onMouseUp={(event)=>{this.onSaveToBaseline(this.baselineDetails)}}>Save to Baseline</button>
                     <button onMouseUp={()=>{this.editing = !this.editing; this.setState({})}}>{this.editing?<span>Save</span>:<span>Edit</span>} Ignore Blocks</button>
                     <div>{this.ignoreBlocks.map(el=><p>{el.left},{el.top},{el.right},{el.bottom}</p>)}</div>
                 </p>
