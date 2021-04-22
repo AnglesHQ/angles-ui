23abb190ee2a1a9eb720d16d3dcc83763a17c16f
Sergio Neves Barros <s.nevesbarros@googlemail.com>
2021-04-22 20:37:18 +0100

    adding component and environment to build summary table

--- src/components/tables/BuildSummary.js
+++ src/components/tables/BuildSummary.js
@@ -14,6 +14,8 @@ class BuildSummary extends Component {
 
   render() {
     const { build, screenshots, openModal } = this.props;
+    const buildComponent = build.team.components
+      .filter((component) => component._id === build.component)[0];
     return (
       <table className="table">
         <thead className="thead-dark">
@@ -29,14 +31,26 @@ class BuildSummary extends Component {
             <td>{build.result.PASS}</td>
           </tr>
           <tr>
+            <th scope="row">Component</th>
+            <td>{ buildComponent.name}</td>
+            <th scope="row">Fail</th>
+            <td>{build.result.FAIL}</td>
+          </tr>
+          <tr>
+            <th scope="row">Environment</th>
+            <td>{build.environment.name}</td>
+            <th scope="row">Error</th>
+            <td>{build.result.ERROR}</td>
+          </tr>
+          <tr>
             <th scope="row">Start</th>
             <td>
               <Moment format="DD-MM-YYYY HH:mm:ss">
                 {build.start}
               </Moment>
             </td>
-            <th scope="row">Fail</th>
-            <td>{build.result.FAIL}</td>
+            <th scope="row">Skipped</th>
+            <td>{build.result.SKIPPED}</td>
           </tr>
           <tr>
             <th scope="row">Finish</th>
@@ -45,25 +59,6 @@ class BuildSummary extends Component {
                 {build.end}
               </Moment>
             </td>
-            <th scope="row">Error</th>
-            <td>{build.result.ERROR}</td>
-          </tr>
-          <tr>
-            <th scope="row">Duration</th>
-            <td>{getDuration(build)}</td>
-            <th scope="row">Skipped</th>
-            <td>{build.result.SKIPPED}</td>
-          </tr>
-          <tr>
-            <th scope="row">
-              <span>Keep flag set </span>
-              <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">By setting the &quot;keep&quot; flag the build will not be removed by the nightly clean-up.</Tooltip>}>
-                <span>
-                  <i className="far fa-question-circle" />
-                </span>
-              </OverlayTrigger>
-            </th>
-            <td>{ build.keep ? 'true' : 'false'}</td>
             <th scope="row">Screenshots</th>
             <td>
               {
@@ -87,6 +82,19 @@ class BuildSummary extends Component {
                   : 0
               }
             </td>
+          </tr>
+          <tr>
+            <th scope="row">Duration</th>
+            <td>{getDuration(build)}</td>
+            <th scope="row">
+              <span>Keep flag set </span>
+              <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">By setting the &quot;keep&quot; flag the build will not be removed by the nightly clean-up.</Tooltip>}>
+                <span>
+                  <i className="far fa-question-circle" />
+                </span>
+              </OverlayTrigger>
+            </th>
+            <td>{ build.keep ? 'true' : 'false'}</td>
           </tr>
         </tbody>
       </table>
