5ae02c4e8b4ae960be1e2c8e19b1db546dd89200
Sergio Neves Barros <s.nevesbarros@googlemail.com>
2021-11-28 21:13:15 +0000

    added versions to about page

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,6 +1,11 @@
 import { React, Component } from 'react';
 import 'react-multi-carousel/lib/styles.css';
 import './Default.css';
+import axios from 'axios';
+import { AnglesRequests } from 'angles-javascript-client';
+import Table from 'react-bootstrap/Table';
+
+const { version } = require('../../../package.json');
 
 class AboutPage extends Component {
   constructor(props) {
@@ -8,9 +13,18 @@ class AboutPage extends Component {
     this.state = {
       //
     };
+    this.anglesRequests = new AnglesRequests(axios);
+  }
+
+  componentDidMount() {
+    this.anglesRequests.getVersions()
+      .then((versions) => {
+        this.setState({ versions });
+      });
   }
 
   render() {
+    const { versions } = this.state;
     return (
       <div>
         <h1>About Angles</h1>
@@ -32,6 +46,37 @@ class AboutPage extends Component {
           For more information about Angles go to page:
           <br />
           <a href="https://angleshq.github.io/" rel="noreferrer" target="_blank">https://angleshq.github.io/</a>
+        </div>
+        <div>
+          <br />
+          {
+            versions ? (
+              <Table className="about-versions-table" size="sm">
+                <tr className="thead-dark">
+                  <th scope="col">Component</th>
+                  <th scope="col">Version</th>
+                </tr>
+                <tbody>
+                  <tr>
+                    <td>Angles Frontend</td>
+                    <td>{version}</td>
+                  </tr>
+                  <tr>
+                    <td>Angles Backend</td>
+                    <td>{versions.angles}</td>
+                  </tr>
+                  <tr>
+                    <td>Node</td>
+                    <td>{versions.node}</td>
+                  </tr>
+                  <tr>
+                    <td>Mongo</td>
+                    <td>{versions.mongo}</td>
+                  </tr>
+                </tbody>
+              </Table>
+            ) : null
+          }
         </div>
       </div>
     );
