ab292973dc55dc020b1a4256e9239cf6dce47b23
sarm333 <sarm333@gmail.com>
2023-12-05 15:35:51 +0000

    did not modify eventKey

--- src/containers/App.js
+++ src/containers/App.js
@@ -199,7 +199,7 @@ const App = function (props) {
                 <Nav.Menu eventKey="4" icon={<GlobalIcon style={{ fontSize: '20px', height: '20px' }} />} title={<FormattedMessage id="nav.language" />}>
                   <Nav.Item eventKey="4-1" onClick={() => setLanguage('en')} href="/">English</Nav.Item>
                   <Nav.Item eventKey="4-2" onClick={() => setLanguage('nl')} href="/">Nederlands</Nav.Item>
-                  <Nav.Item eventKey="4-2" onClick={() => setLanguage('th')} href="/">ไทย</Nav.Item>
+                  <Nav.Item eventKey="4-3" onClick={() => setLanguage('th')} href="/">ไทย</Nav.Item>
                 </Nav.Menu>
                 <Nav.Menu eventKey="5" icon={<CgDarkMode />} title={<FormattedMessage id="nav.theme" />}>
                   <Nav.Item eventKey="5-1" onClick={() => setTheme('light')}><FormattedMessage id="nav.theme.light" /></Nav.Item>
