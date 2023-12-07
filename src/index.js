084752ac732a17f4b4c0ca701524d52d5fc6ec5d
Gary Tsang <GaryTsang@users.noreply.github.com>
2023-12-07 13:52:51 +0000

    removed hardcoded language json imports for a more dynamic experience

--- src/index.js
+++ src/index.js
@@ -4,27 +4,20 @@ import Cookies from 'js-cookie';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import messagesEN from './translations/en.json';
-import messagesTH from './translations/th.json';
-import messagesNL from './translations/nl.json';
-
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
-
-const messages = {
-  en: messagesEN,
-  nl: messagesNL,
-  th: messagesTH,
-};
 let language = navigator.language.split(/[-_]/)[0];
 const cookieLanguage = Cookies.get('language');
 if (cookieLanguage) {
   language = cookieLanguage;
 }
+const messages = {};
+messages.en = await import('./translations/en.json');
+messages[language] = await import(`./translations/${language}.json`);
 language = language in messages ? language : 'en';
 Cookies.set('language', language);
 
